2429f945a85a805a925147bbf56094d6d9bf226a
Coen M (Scribblon) <coenmeulenkamp@gmail.com>
2020-08-18 14:29:44 +0200

    Readme and minor adjustment

--- bin/tech_ground_lab_base.ts
+++ bin/tech_ground_lab_base.ts
@@ -4,4 +4,5 @@ import * as cdk from '@aws-cdk/core';
 import { TechGroundLabBaseStack } from '../lib/tech_ground_lab_base-stack';
 
 const app = new cdk.App();
-new TechGroundLabBaseStack(app, 'TechGroundLabBaseStack');
+const stack = new TechGroundLabBaseStack(app, 'TechGroundLabBaseStack');
+cdk.Tag.add(stack, 'Source', 'Autogenned');
